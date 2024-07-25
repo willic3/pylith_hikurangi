1746172c3fad540ff2b9db42cb243896e147df46
Charles Williams <C.Williams@gns.cri.nz>
2024-07-25 16:20:43 +0200

    Fixed stupid indexing error in my first attempt to use borehole coordinates for profile orientation.

--- profile_2d/mesh/extract_profiles.py
+++ profile_2d/mesh/extract_profiles.py
@@ -19,9 +19,14 @@ profileCoordsTM = np.array([[3.436441782295286976e+04, -1.771862404823477482e+04
                             [9.946172787998057174e+03, -3.044082226190067104e+03]], dtype=np.float64)
 
 # Profile orientation.
-# dx = profileCoordsTM[0,0] - profileCoordsTM[1,0]
-# dy = profileCoordsTM[1,1] - profileCoordsTM[1,0]
-# angle = math.atan2(dy, dx)
+dx = profileCoordsTM[0,0] - profileCoordsTM[1,0]
+dy = profileCoordsTM[0,1] - profileCoordsTM[1,1]
+angle = math.atan2(dy, dx)
+
+# Rotation matrix.
+ca = math.cos(angle)
+sa = math.sin(angle)
+rotMat = np.array([[ca, sa], [-sa, ca]], dtype=np.float64)
 
 # Fault profile.
 faultDat = np.loadtxt(inFaultFile, skiprows=1, dtype=np.float64)
@@ -31,16 +36,6 @@ faultZ3d = faultDat[:,-1]
 sortInds = np.argsort(faultX3d)
 fault3d = np.column_stack((faultX3d[sortInds], faultY3d[sortInds], faultZ3d[sortInds]))
 fault2d = fault3d[:,0:2] - refCoordTM
-
-# Get rotation angle from profile rather than borehole coordinates.
-dx = fault2d[-1,0] - fault2d[0,0]
-dy = fault2d[-1,1] - fault2d[0,1]
-angle = math.atan2(dy, dx)
-
-# Rotation matrix.
-ca = math.cos(angle)
-sa = math.sin(angle)
-rotMat = np.array([[ca, sa], [-sa, ca]], dtype=np.float64)
 
 faultRot = np.dot(fault2d, rotMat.transpose())
 
